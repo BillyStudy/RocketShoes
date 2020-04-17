ca2fb800badf6748f25390cc6faf9adcdd28b9ef
BillyStudy <billystudy@hotmail.com>
2020-04-17 17:40:22 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import {ProductList} from './styles';
+import {MdAddShoppingCart} from 'react-icons/md';
+
 export default function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  );
+
+    return (
+        <ProductList>
+            {/* Produto */}
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
+                    alt="croquis"
+                />
+                <strong>Crocs estiloso</strong>
+                <span>R$ 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff" size={16} />  3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span> 
+                </button>
+            </li>
+        </ProductList>
+    );
 }
