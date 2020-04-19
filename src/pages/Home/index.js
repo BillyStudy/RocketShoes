391f5ff4333c5e1fb92522af66d1340655d7d9ad
BillyStudy <billystudy@hotmail.com>
2020-04-19 20:24:45 -0300

    Get products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,102 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {ProductList} from './styles';
 import {MdAddShoppingCart} from 'react-icons/md';
 
+import api from '../../services/api';
+import {formatPrice} from '../../util/format';
+
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    async function getProducts() {
+        const response = await api.get('products');
+        const data = response.data.map(prod => ({
+            ...prod,
+            priceFormatted: formatPrice(prod.price)
+        }))
+        setProducts(data);
+    }
+    useEffect(() => getProducts(), []);
 
     return (
         <ProductList>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
-            {/* Produto */}
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/sandalia-crocs-classic/12/117-0092-012/117-0092-012_zoom2.jpg?ims=326x"
-                    alt="croquis"
-                />
-                <strong>Crocs estiloso</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff" size={16} />  3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span> 
-                </button>
-            </li>
+            {products.map((prod) => (
+                <li key={prod.id}>
+                    <img
+                        src={prod.image}
+                        alt={prod.title}
+                    />
+                    <strong>{prod.title}</strong>
+                    <span>{prod.priceFormatted}</span>
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart color="#fff" size={16} /> 3
+                        </div>
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
         </ProductList>
     );
 }
